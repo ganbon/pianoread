4adc492ffee0665373d8a46bd63c3b22211659f0
shake0917 <s21t326@kagawa-u.ac.jp>
2021-12-17 20:51:54 +0900

    ファイル追加

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ from scipy.io import wavfile
 FREQ = 261.626          # 生成するサイン波の周波数（note#60 C4 ド）
 SAMPLE_RATE = 44100     # サンプリングレート
 # 16bitのwavファイルを作成
-wavfile.write("do.wav", SAMPLE_RATE,
+wavfile.write("shake.wav", SAMPLE_RATE,
               (np.sin(np.arange(SAMPLE_RATE) * FREQ * np.pi * 2 / SAMPLE_RATE) * 32767.0).astype(np.int16))
