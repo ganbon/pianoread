209a5c562ce6eb862dd98fed68f65ba25a4121d5
gomagom <s20t331@stu.kagawa-u.ac.jp>
2022-01-24 14:59:10 +0900

    change

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,12 @@ def _triangle_stair(freq, t):
     s = np.abs((2 * t * freq - 1 / 2) % 2 - 1) * 16 + 0.5
     return s.astype("int64") / 8 - 1
 
+# 階段三角波（ファミコン）
+def _triangle_stair2(freq, t):
+    s = list(range(16)) + list(range(15, -1, -1))
+    print(s)
+
 x = np.linspace(0, 2, 44100 * 2 + 1)
-y = _triangle_stair(1, x)
-plt.plot(x, y)
-plt.show()
+y = _triangle_stair2(1, x)
+# plt.plot(x, y)
+# plt.show()
